9c164ff8a1784ed3e55d85535374529c3115a77e
SleepyNightMight <bowen.jordan2016@gmail.com>
2020-12-02 15:16:55 -0500

    Project

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float opponentSpeed;
+    public float speedModifier;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+        Debug.Log("Speed Modifier: " + speedModifier);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float adjustedSpeed = opponentSpeed * speedModifier;
+        transform.position += (Vector3.right * adjustedSpeed * Time.deltaTime);
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bound1" || collision.tag == "Bound2")
+        {
+            speedModifier = Random.Range(0.5f, 2);
+            opponentSpeed *= -1.0f;
+
+           
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float playerSpeed;
+    //private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //scoreKeeper = GetComponent<PlayerScoreKeeper>();
+        //rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+        Vector3 xMovement = new Vector3(xValue, 0, 0);
+        //Vector3 moveDirection = new Vector3(xValue, 0.0f, 0.0f);
+        // Vector3.moveDirection = new Vector3(xValue,0.0f,0.0f); 
+        //rb.AddForce(moveDirection, ForceMode2D.Force);
+        transform.position += xMovement;
+
+    }
+   
+}
